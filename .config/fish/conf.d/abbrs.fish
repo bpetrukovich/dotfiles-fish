4659b42ecac652802430be3bb50002f8fff8a5c0
Bogdan Petrukovich <petrukovichbogdan@gmail.com>
2025-12-28 18:59:11 +0300

    refactor fish config

---
+++ .config/fish/conf.d/abbrs.fish
@@ -0,0 +1,4 @@
+abbr -a vi nvim
+abbr -a vim nvim
+abbr -a nvim. 'nvim .'
+abbr -a fshc 'nvim ~/.config/fish/config.fish'
---
+++ .config/fish/conf.d/env.fish
@@ -0,0 +1,4 @@
+set -Ux EDITOR nvim
+set -Ux VISUAL nvim
+set -Ux PAGER less
+set -Ux LANG en_US.UTF-8
