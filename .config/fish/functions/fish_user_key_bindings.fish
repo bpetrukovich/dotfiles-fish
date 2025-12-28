16f2988c8b67ee10208211387e19a18d035f2a62
Bogdan Petrukovich <petrukovichbogdan@gmail.com>
2025-12-28 19:15:12 +0300

    fix bindings

--- .config/fish/functions/fish_user_key_bindings.fish
+++ .config/fish/functions/fish_user_key_bindings.fish
@@ -1,4 +1,9 @@
 function fish_user_key_bindings
+    fish_vi_key_bindings
+
     bind \cf 'multi-sessionizer'
     bind \ce 'script-selector'
+
+    bind -M insert \cf 'multi-sessionizer'
+    bind -M insert \ce 'script-selector'
 end
