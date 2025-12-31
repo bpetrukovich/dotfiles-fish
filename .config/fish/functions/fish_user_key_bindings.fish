486f621bcb2f59649d92c46097ad433998f97503
Bogdan Petrukovich <petrukovichbogdan@gmail.com>
2025-12-31 13:45:36 +0300

    fix atuin history

--- .config/fish/functions/fish_user_key_bindings.fish
+++ .config/fish/functions/fish_user_key_bindings.fish
@@ -6,6 +6,4 @@ function fish_user_key_bindings
 
     bind -M insert \cf 'multi-sessionizer'
     bind -M insert \ce 'script-selector'
-
-    fzf_configure_bindings --git_status= --history= --variables=\e\cv
 end
