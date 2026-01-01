9a25b9fdd45ca77514cb69f0a6c86724a0ba545a
Bogdan Petrukovich <petrukovichbogdan@gmail.com>
2026-01-01 22:35:45 +0300

    remove "done" as it's not stable in wsl

--- setup.sh
+++ setup.sh
@@ -214,7 +214,6 @@ rm -rf kubectl
 curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
 fisher install kidonng/zoxide.fish
 fisher install PatrickF1/fzf.fish
-# fisher install franciscolourenco/done
 
 # gita
 # sudo apt install python3-pip
