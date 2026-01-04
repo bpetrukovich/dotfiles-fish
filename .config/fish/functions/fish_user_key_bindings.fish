0fdf7d101fec1faa1e5034459dd6e196e45e273c
Bogdan Petrukovich <petrukovichbogdan@gmail.com>
2026-01-04 14:26:34 +0300

    remove script-selector bindings

--- .config/fish/functions/fish_user_key_bindings.fish
+++ .config/fish/functions/fish_user_key_bindings.fish
@@ -2,8 +2,6 @@ function fish_user_key_bindings
     fish_vi_key_bindings
 
     bind \cf 'multi-sessionizer'
-    bind \ce 'script-selector'
 
     bind -M insert \cf 'multi-sessionizer'
-    bind -M insert \ce 'script-selector'
 end
